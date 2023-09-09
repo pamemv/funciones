9bff398c94b8888d15d4691c753e592a20e59d9b
Pamela Maturana <pamela.mv22@hotmail.com>
2023-09-09 16:02:17 -0300

    se realizan ejercicio 1 y 2 del desafio

---
+++ 2_arrow.js
@@ -0,0 +1,12 @@
+/*  Transforma la siguiente función a una arrow function de una línea
+
+let suma = function(a, b){
+return a + b
+}
+
+nota: cuando se usan las {} debe ir un return, para que quede de 1 linea luego del => va directo la operación a realizar
+*/
+
+const suma => (a, b) => a + b
+
+
